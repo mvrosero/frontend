5c22846b6100b5c44f69372eee812f46ada30a02
Michaela Vera G. Rosero <mvrosero@gbox.ncf.edu.ph>
2024-03-02 20:14:41 +0800

    frontend latest commit 6

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,14 +1,54 @@
 import * as React from "react";
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+
+import "./Contact.css";
+
+import phone from '../components/images/phone.png';
+import social from '../components/images/social.png';
+import email from '../components/images/email.png';
 
 
 function Contact() {
+  const cardsData = [
+    {
+      title: "Phone Number",
+      text: "09055753247",
+      imageSrc: phone
+    },
+    {
+      title: "Social Media",
+      text: "Facebook: Micha Rosero\nInstagram: @michaelavera_\nTwitter: @meechaelarosero",
+      imageSrc: social
+    },
+    {
+      title: "Email",
+      text: "Personal: michaelarosero@gmail.com",
+      imageSrc: email
+    }
+  ];
+
   return (
-    <>
-      <h1>Contact</h1>
-      <p>Hello there.<br />How do you do?</p>
-    </>
+    <div className="contact-body">
+      <h1 className="text-center mb-4">CONTACT</h1>
+      <div className="d-flex justify-content-center">
+        <Row xs={1} md={3} className="g-4">
+          {cardsData.map((card, idx) => (
+            <Col key={idx}>
+              <Card className="contact-card">
+                <Card.Img variant="top" src={card.imageSrc} />
+                <Card.Body>
+                  <Card.Title>{card.title}</Card.Title>
+                  <Card.Text>{card.text}</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </div>
+    </div>
   );
- }
- 
+}
 
 export default Contact;
